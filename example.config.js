fa31fd836cf2d8624dfb48057f6b82a14fcac213
Remaii <remy.thidet@hotmail.fr>
2017-10-25 14:00:31 +0200

    add example config

---
+++ example.config.js
@@ -0,0 +1,44 @@
+'use strict'
+
+var duree = 300000;// millisecondes
+var dec = (duree / 1000);// secondes
+var url = 'url to request and return { total: Number, onb: Number }';
+
+var flash = function(led, timeval, time) {
+	console.log('Flash Times:' + timeval + 'ms, ' + time + 'ms.');
+	var inter = null;
+
+	function showInter() {
+		if (led.readSync() === 0) {
+			led.writeSync(1);
+		} else {
+			led.writeSync(0);
+		}
+	}
+
+	function showTime() {
+	  clearInterval(inter);
+	  if (led.readSync() === 1) {
+			led.writeSync(0);
+		}
+	}
+
+	inter = setInterval(showInter, timeval);
+  setTimeout(showTime, time);
+}
+
+var all = {
+	duree: duree,
+	decompte: dec,
+	options: {
+    method: 'GET',
+    url: url,
+    rejectUnauthorized: false,
+    headers: {
+    	'Content-Type':'x-www-form-urlencoded'
+    }
+  },
+	flash: flash
+}
+
+module.exports = all;
