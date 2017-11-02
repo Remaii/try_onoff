eb2a1031468ed755a3081ba144f51c6ed4a194b1
Remaii <remy.thidet@hotmail.fr>
2017-11-02 23:43:07 +0100

    led function

---
+++ led.js
@@ -0,0 +1,187 @@
+'use strict'
+
+var config = require('../config');
+
+var flash = function(led, timeval, time) {
+	console.log('Flash, intervals: ' + timeval + 'ms, duration: ' + time + 'ms.');
+	
+	if (!led || Array.isArray(led)) {
+		console.log('Error, the led passed is Empty or is Array. Function Flash take one led in first params');
+		return ;
+	}
+
+	var inter = null;
+	var interval = (timeval >= 100 ? timeval : 100);
+	var duration = (time <= config.duree ? time : config.duree);
+
+	function showInter() {
+		if (led.readSync() === 0) {
+			led.writeSync(1);
+		} else {
+			led.writeSync(0);
+		}
+	};
+
+	function showTime() {
+	  clearInterval(inter);
+	  if (led.readSync() === 1) {
+			led.writeSync(0);
+		}
+	};
+	inter = setInterval(showInter, interval);
+  setTimeout(showTime, duration);
+};
+
+var pingpong = function(led, timeval, time) {
+	console.log('Pingpong, intervals: ' + timeval + 'ms, duration: ' + time + 'ms.');
+	if (!led || !Array.isArray(led)) {
+		console.log('Error, the led passed is Empty or not an Array. Function PingPong take 2+ leds in first params in Array');
+		return ;
+	}
+	var inter = null;
+	var interval = (timeval >= 100 ? timeval : 100);
+	var duration = (time <= config.duree ? time : config.duree);
+	var prev = {};
+
+	function showInter() {
+		led.forEach(function(elem , index) {			
+			if (index != 0) {
+				if (elem.readSync() === 0) {
+					elem.writeSync(1);
+				} else {
+					elem.writeSync(0);
+				}
+			} else {
+				if (elem.readSync() === 0) {
+					elem.writeSync((prev.state === 1 ? 0 : 1));
+				} else {
+					elem.writeSync((prev.state === 1 ? 0 : 1));
+				}
+			}
+			console.log(elem.name, elem.state, '<=>', prev.name, prev.state);
+			prev = elem;
+		});
+	};
+
+	function showTime() {
+	  clearInterval(inter);
+	  led.forEach(function(elem) {
+		  if (elem.readSync() === 1) {
+				elem.writeSync(0);
+			}
+	  });
+	};
+
+	inter = setInterval(showInter, interval);
+  setTimeout(showTime, duration);
+};
+
+function fix(led, time) {
+	console.log('Fix, duration: ' + time + 'ms.');
+	if (!led) {
+		console.log("Error: fix function need led to perform");
+		return ;
+	}
+	var duration = (time <= config.duree ? time : config.duree);
+	
+	function showInter() {
+		if (led.readSync() === 0) {
+			led.writeSync(1);
+		}
+	};
+	function showTime() {
+	  if (led.readSync() === 1) {
+			led.writeSync(0);
+		}
+	};
+
+	showInter();
+  setTimeout(showTime, duration);	
+};
+
+function solder(led, time) {
+	console.log('Fix, duration: ' + time + 'ms.');
+	if (!led || !Array.isArray(led)) {
+		console.log("Error: solder function need Array of led to perform");
+		return ;
+	}
+	var duration = (time <= config.duree ? time : config.duree);
+	
+	function showInter() {
+		led.forEach(function(elem) {
+			if (elem.readSync() === 0) {
+				elem.writeSync(1);
+			}
+		});
+	};
+	function showTime() {
+		led.forEach(function(elem) {
+		  if (elem.readSync() === 1) {
+				elem.writeSync(0);
+			}
+		});
+	};
+
+	showInter();
+  setTimeout(showTime, duration);	
+};
+
+function powerOff(leds) {
+	for (var i = 0; i < leds.length; i++) {
+		leds[i].writeSync(0);
+	}
+};
+
+// UNIT TEST without Raspberry Pi, uncomment all under this text
+var randOnb = Number.parseInt(Math.random() * 50);
+var randTotal = (Number.parseInt(Math.random() * 50) + randOnb);
+
+var readSync = function() {
+	console.log(this.name, 'readSync', this.state);
+	return this.state;
+};
+var writeSync = function(value) {
+	console.log(this.name, 'writeSync', this.state, 'to', value);
+	return this.state = value;
+};
+var fakeRequest = function() {
+	var total = randTotal;
+	var onb = randOnb;
+	var grp = 5;
+
+	randTotal += 1;
+	// randOnb += 1;
+	
+	var json = {
+		total:total,
+		onb:onb,
+		grp:grp,
+	}
+	return JSON.stringify(json);
+};
+var fakeLed = function(name) {
+	return {
+		state: 0,
+		name: name,
+		readSync: readSync,
+		writeSync: writeSync
+	};
+};
+// all.pingpong([fakeLed("led1"), fakeLed("led2")], 500, 10000);
+// all.flash(fakeLed("led3"), 500, 10000);
+// all.solder([fakeLed("led4"), fakeLed("led5")], 10000);
+// all.fix(fakeLed("led6"), 10000);
+
+// END OF UNIT TEST
+
+var all = {
+	pingpong: pingpong, // ([leds], timeval, time)
+	solder: solder, // ([leds], time)
+	fix: fix, // (led, time)
+	flash: flash, // (led, timeval, time)
+	powerOff: powerOff, // (leds)
+	
+	fakeRequest: fakeRequest, // 
+	fakeLed: fakeLed, //
+};
+module.exports = all;
--- script.js
+++ script.js
@@ -2,11 +2,21 @@
 
 // Depend
 var config = require('../config');
+var show = require('./led');
 var request = require('request');
-var gpio = require('onoff').Gpio;
-// GPIO
-var Led1 = new gpio(config.pin1, 'out');
-var Led2 = new gpio(config.pin2, 'out');
+
+// var gpio = require('onoff').Gpio;
+
+// // GPIO
+// var Led1 = new gpio(config.pin1, 'out');
+// var Led2 = new gpio(config.pin2, 'out');
+
+// Begin of Test without Raspberry Pi
+var Led1 = show.fakeLed("GPIO 17");
+var Led2 = show.fakeLed("GPIO 27");
+var leds = [Led1, Led2];
+// End of Test without Raspberry Pi
+
 // Global var
 var save = {};
 var debug = (process.env.DEBUG === 'true' ? true : false) || false;
@@ -25,79 +35,112 @@ function deterDiff(data, key) {
   }
 }
 
-function setLed(led, data, key) {
+function setLed(data, key) {
   var diff = deterDiff(data, key)
   if ('onb' === key) {
     if (diff > 0) {
-      config.flash(led, 100, 10000);
+      show.flash(leds, 100, 10000);
     } else if (diff < 0) {
-      
+      show.solder(leds, 10000);
     } else {
-      led.writeSync(0)
+      show.powerOff(leds[0]);
     }
+    save[key] = data[key];
   } else if ('total' === key) {
-
+    if (diff > 0) {
+      show.flash(leds, 100, 10000);
+    } else if (diff < 0) {
+      show.pingpong(leds, 100, 10000);
+    } else {
+      show.powerOff(leds[1]);
+    }
+    save[key] = data[key];
+  } else if ('grp' === key) {
+    if (diff > 0) {
+      show.pingpong(leds, 200, 10000);
+    } else if (diff < 0) {
+      show.fix(led[1], 100, 10000);
+    } else {
+      show.powerOff(leds[1]);
+    }
+    save[key] = data[key];
   } else {
-    led.writeSync(0);
+    show.powerOff(leds);
   }
 }
 
-function verifTotal(led, data, key) {
-  if (data[key] > save.total) {
-    if (debug) { console.log('data[key] > save.total', data[key], key); }
+function checkDifference(leds, data) {
+  var mandatory = ["total", "onb", "grp"];
 
-    if (led.readSync() === 0) {
-      led.writeSync(1);
-    }
-  } else {
-    if (debug) { console.log('data[key] <= save.total', data[key], key); }
-
-    if (led.readSync() === 1) {
-      led.writeSync(0);
-    }
+  for (var i = 0; i < mandatory.length; i++) {
+    setLed(leds, mandatory[i])
+    // save[mandatory[i]] = data[mandatory[i]];
   }
-  save.total = data[key];
 }
 
-function verifOnb(led, data, key) {
-  if (data[key] < save.onb) {
-    if (debug) { console.log('data[key] < save.onb', data[key], key); }
+// function verifTotal(led, data, key) {
+//   if (data[key] > save.total) {
+//     if (debug) { console.log('data[key] > save.total', data[key], key); }
 
-    if (led.readSync() === 0) {
-      led.writeSync(1);
-    }
-  } else if (data[key] > save.onb) {
-    if (debug) { console.log('data[key] > save.onb', data[key], key); }
+//     if (led.readSync() === 0) {
+//       led.writeSync(1);
+//     }
+//   } else {
+//     if (debug) { console.log('data[key] <= save.total', data[key], key); }
 
-    config.flash(led, 250, (time / 2));
-  } else {
-    if (debug) { console.log('data[key] >= save.onb', data[key], key); }
+//     if (led.readSync() === 1) {
+//       led.writeSync(0);
+//     }
+//   }
+//   save.total = data[key];
+// }
 
-    if (led.readSync() === 1) {
-      led.writeSync(0);
-    }
-  }
-  save.onb = data[key];
-}
+// function verifOnb(led, data, key) {
+//   if (data[key] < save.onb) {
+//     if (debug) { console.log('data[key] < save.onb', data[key], key); }
+
+//     if (led.readSync() === 0) {
+//       led.writeSync(1);
+//     }
+//   } else if (data[key] > save.onb) {
+//     if (debug) { console.log('data[key] > save.onb', data[key], key); }
+
+//     show.flash(led, 250, (time / 2));
+//   } else {
+//     if (debug) { console.log('data[key] >= save.onb', data[key], key); }
+
+//     if (led.readSync() === 1) {
+//       led.writeSync(0);
+//     }
+//   }
+//   save.onb = data[key];
+// }
 
 function getDataAndCompare() {
   request(config.options, function(err, result, body) {
-    if (err) { console.log('Request Error:', err); }
-    console.log('Response Body:', body);
+    if (err) { 
+      if (debug) {
+        // console.log('Request Error:', err);
+      }
+    }
     
-    var tmp = JSON.parse(body);
-    
+    if (body) {
+      console.log('Response Body:', body);
+      var tmp = JSON.parse(body);
+    } else {
+      var tmp = JSON.parse(show.fakeRequest());
+      console.log('fakeRequest send :', tmp);
+    }
+    console.log(save, save.total);
     if (save.total && save.onb) {
       if (debug) { console.log('save is fully define', save); }
-      verifTotal(Led1, tmp, 'total');
-      verifOnb(Led2, tmp, 'onb');
+      checkDifference(leds, tmp);
     }
     else {
-      if (debug) { console.log('firstLaunch save:', save); }
-      
       save = tmp;
-      config.flash(Led1, 500, (time / 2));
-      config.flash(Led2, 250, (time / 2));
+      
+      if (debug) { console.log('firstLaunch save:', save); }
+      checkDifference(leds, tmp);
     }
   });
 }
