7fae495d0542fe74fa8ca3686bb9713115123b85
Remaii <remy.thidet@hotmail.fr>
2017-11-04 23:04:29 +0100

    rPi

--- app.js
+++ app.js
@@ -10,20 +10,19 @@ var config = require('../config1'),
 // Globals variables
 var leds = [],
 	status = {},
-	launch = false;
+	launch = false,
+	debug = (process.env.DEBUG === 'true' ? true : false) || false;
 
 function determineLeds() {
-	console.log('deter');
 	var tmp = [];
 	_.each(config.pins, function(value) {
 		tmp.push(new gpio(value, 'out'));
 	});
-	console.log('tmp', tmp);
 	return tmp;
 }
 
 function setStatus() {
-	console.log('status', status);
+	if (debug) { console.log('status', status); }
 	_.each(status, function(elem) {
 		console.log(elem);
 		if (config.pins.indexOf(elem.number) >= 0) {
@@ -34,8 +33,6 @@ function setStatus() {
 
 function getStatus(time) {
 	request(config.options_status, function(err, result, body) {
-    var tmp = {};
-
     if (err) {
       if (debug) {
         console.log('Request Error:', err);
@@ -52,11 +49,9 @@ function getStatus(time) {
 }
 
 if (leds.length <= 0) {
-	console.log('post deter');
 	leds = determineLeds();
 }
 
 if (!launch && leds.length > 0) {
-	console.log('leds', leds);
 	getStatus();
 }
