da79ecf6c0dcfefb0999a4e3e8b650568ed3494e
Remaii <remy.thidet@hotmail.fr>
2017-11-05 01:53:29 +0100

    rPi

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ function setStatus() {
 	_.each(status, function(elem) {
 		_.each(leds, function(led) {
 			if (led.gpio === elem.number) {
+				console.log((elem.state === 'true' ? 1 : 0), led.readSync(), (elem.state === 'true' ? 1 : 0) !== led.readSync());
 				if ((elem.state === 'true' ? 1 : 0) !== led.readSync()) {
 					console.log('setStatus of:', elem.name, 'gpio#:', elem.number, 'state:', elem.state);
 					led.writeSync((elem.state === "true" ? 1 : 0));
